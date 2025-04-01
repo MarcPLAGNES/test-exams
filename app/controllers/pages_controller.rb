066042eee188051dfd62fc79b4d8335f41762b6b
Marc PLAGNES <m.plagnes@hotmail.fr>
2025-04-01 19:22:56 +0200

    Modification Test exams

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    @hide_upperbar = true
+    @all_users = User.where(username: ['erwann', 'alvaro', 'victoire', 'marc']) # Vérifie que ces utilisateurs existent
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,30 @@ require 'faker'
   # Sauvegarde les données dans la base de données
   Restaurant.create(name: nom, address: address, rating: rating)
 end
+
+# Users seed
+erwann = User.create!(
+  username: "edefoy",
+  password: "pwd123",
+  email: "erwann@gamil.com"
+)
+
+alvaro = User.create!(
+  username: "vancastle",
+  password: "pwd123",
+  email: "alvaro@gamil.com"
+)
+
+victoire = User.create!(
+  username: "VictoireBB",
+  password: "pwd123",
+  email: "victoire@gamil.com"
+)
+
+marc = User.create!(
+  username: "MarcPLAGNES",
+  password: "pwd123",
+  email: "marc@gamil.com"
+)
+
+all_users = [erwann, alvaro, victoire, marc]
